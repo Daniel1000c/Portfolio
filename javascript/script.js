0a98196324db93c3ba35ca1756dd377e2cdbf410
Daniel Velez <velezdaniel516@gmail.com>
2024-05-08 13:21:37 -0400

    add redirect arrow

--- javascript/script.js
+++ javascript/script.js
@@ -22,3 +22,25 @@ function handleScroll(){
 
 // Attach scroll event listener
 window.addEventListener('scroll',handleScroll);
+
+//get arrow element
+var arrow = document.getElementById("scroll-to-top");
+
+// Add scroll event listener
+window.addEventListener("scroll",function() {
+    //if user scrolls down more than 20px, show the arrow, otherwise hide it
+    if (this.document.body.scrollTop > 20 || this.document.documentElement.scrollTop > 20){
+        arrow.style.display = "block";
+    } else {
+        arrow.style.display = "none";
+    }
+});
+
+// add a click event listener to the arrow
+arrow.addEventListener("click",function(){
+    // scroll to the top of the page smoothly
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
