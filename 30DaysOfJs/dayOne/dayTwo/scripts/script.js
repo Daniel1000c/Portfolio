9129137b6ce32e29fd6894d89649521702b85af4
Daniel Velez <velezdaniel516@gmail.com>
2024-10-31 16:22:27 -0700

    added daytwo folder

---
+++ 30DaysOfJs/dayOne/dayTwo/scripts/script.js
@@ -0,0 +1,30 @@
+// declare variable challenge
+let challenge = '30 Days of JavaScript';
+
+//Print string out
+console.log(challenge);
+
+//Print out the length of the string
+console.log(challenge.length);
+
+//Change challenge variable to uppercase
+console.log(challenge.toUpperCase());
+
+//Change challenge variable to lowercase
+console.log(challenge.toLowerCase());
+
+//Cut the first word of the string
+console.log(challenge.substring(2,21));
+
+//Cut out the phrase days of javascript
+console.log(challenge.substring(0,2));
+
+//Check to see if string has a word script using include
+console.log(challenge.includes('Script'));
+
+//Split the string into the array
+console.log(challenge.split());
+
+//Split the string into spaces
+console.log(challenge.split(' '));
+
