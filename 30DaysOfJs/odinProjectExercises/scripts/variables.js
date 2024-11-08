06dc5ac40d7d1381a718fcf183172be41021a8ca
Daniel Velez <velezdaniel516@gmail.com>
2024-11-08 15:01:32 -0800

    added odin project folder

---
+++ 30DaysOfJs/odinProjectExercises/scripts/variables.js
@@ -0,0 +1,24 @@
+//add two variables together
+console.log(20+97);
+
+//Add six different numbers using console.log
+console.log(6+24+35+20+2+35);
+
+//Print out statement(4 + 6 + 9) / 77
+console.log((4 + 6 + 9) / 77);
+
+//Declare variable
+let a = 10;
+
+console.log(a);
+
+console.log(9*a);
+
+//Declare a const
+const max = 57;
+
+let actual = max - 13;
+
+let percentage = actual / max
+
+console.log(percentage);
