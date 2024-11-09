b8666ade47b65ee6ed302dee07cb96c00ddb278d
Daniel Velez <velezdaniel516@gmail.com>
2024-11-09 13:39:57 -0800

    added lesson 4 folder

---
+++ 30DaysOfJs/odinProjectExercises/scripts/functions.js
@@ -0,0 +1,21 @@
+//Create a add seven function
+function add7 (num1) {
+    return num1 + 7
+}
+
+//show that result to user
+console.log(add7(10));
+
+//Create a multiply function
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+//Show function result
+
+console.log(multiply(5,2));
+
+//Create a capitalize function
+function capitalize() {
+    
+}
